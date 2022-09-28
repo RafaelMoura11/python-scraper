1a3b85713a29be65bface51bb81bb0b0022a2592
Rafael <rafaelmoura7033@gmail.com>
2022-09-28 20:02:15 -0300

    Requisito 04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -35,26 +35,20 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     selector = Selector(html_content)
-    url = selector.css("""head link[rel="canonical"]::attr(href)""").get()
-    title = selector.css("h1.entry-title::text").get()
-    timestamp = selector.css("li.meta-date::text").get()
-    author = selector.css("li.meta-author span.author a::text").get()
-    category = selector.css("span.label::text").get()
-    comments_count = selector.css("article.comment-body").getall()
-    summary = "".join(
-            selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
-        ).strip()
-    tags = selector.css(".post-tags a::text").getall(),
-
+    # Referencias: Repositorio Breno5g
     return {
-        "url": url,
-        "title": title,
-        "timestamp": timestamp,
-        "writer": author,
-        "comments_count": len(comments_count),
-        "summary": summary,
-        "tags": tags,
-        "category": category,
+        "url": selector.css("""
+        head link[rel="canonical"]::attr(href)""").get(),
+        "timestamp": selector.css("li.meta-date::text").get(),
+        "writer": selector.css("li.meta-author span.author a::text").get(),
+        "comments_count": len(selector.css("article.comment-body").getall()),
+        "summary": "".join(
+            selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+        ).strip(),
+        "category": selector.css("span.label::text").get(),
+        "title": selector.css("h1.entry-title::text").get().replace(
+            u'\xa0\xa0\xa0', u''),
+        "tags": selector.css(".post-tags a::text").getall(),
     }
 
 
