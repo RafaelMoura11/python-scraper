9760d49c79653ebb5a711b96c9a06d125a0ed4da
Rafael <rafaelmoura7033@gmail.com>
2022-09-28 17:28:00 -0300

    Requisito 03

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,7 +27,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_button = selector.css(".next.page-numbers::attr(href)").get()
+    return next_page_button
 
 
 # Requisito 4
