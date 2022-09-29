c778fe57701e4dfb12d710c765641b0b67db3586
Rafael <rafaelmoura7033@gmail.com>
2022-09-29 10:04:37 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,18 @@
+from tech_news.database import search_news
+from operator import itemgetter
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    all_news = search_news({})
+    sorted_news = sorted(all_news, key=itemgetter('comments_count'), reverse=True)
+    titles_and_urls = []
+    for new in sorted_news:
+        titles_and_urls.append((new["title"], new["url"]))
+        if len(titles_and_urls) > 5:
+            return titles_and_urls[:5]
+    return titles_and_urls
+
 
 
 # Requisito 11
