679d08cbc236d9f731889b04fab8ebaca026c18b
Rafael <rafaelmoura7033@gmail.com>
2022-09-29 10:26:48 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -6,15 +6,13 @@ from operator import itemgetter
 def top_5_news():
     """Seu código deve vir aqui"""
     all_news = search_news({})
-    #Source: https://stackoverflow.com/questions/72899/how-do-i-sort-a-list-of-dictionaries-by-a-value-of-the-dictionary
-    #Serviu para aprender a organizar lista de dicts
     sorted_news = sorted(
         all_news, key=itemgetter('comments_count'), reverse=True)
     titles_and_urls = []
     for new in sorted_news:
         titles_and_urls.append((new["title"], new["url"]))
-        if len(titles_and_urls) > 5:
-            return titles_and_urls[0:5]
+    if len(titles_and_urls) > 5:
+        return titles_and_urls[0:5]
     return titles_and_urls
 
 
