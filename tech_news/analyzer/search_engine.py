7cbd70d71da2282a1d0ab5c51bdcb7abd9bcb443
Rafael <rafaelmoura7033@gmail.com>
2022-09-29 09:27:09 -0300

    Requisito 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 # Requisito 6
 from tech_news.database import search_news
+from datetime import datetime
 
 
 def search_by_title(title):
@@ -15,6 +16,16 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+    db_query = {"timestamp": {"$regex": date, "$options": "i"}}
+    titles_and_urls = []
+    news_by_title = search_news(db_query)
+    for new in news_by_title:
+        titles_and_urls.append((new["title"], new["url"]))
+    return titles_and_urls
 
 
 # Requisito 8
